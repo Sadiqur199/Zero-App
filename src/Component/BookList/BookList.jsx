b308f371da0f6d9e0ceeb3f546832bdd6f62f7c7
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-10-15 01:32:23 +0600

    create State is managed using useState hooks

--- src/Component/BookList/BookList.jsx
+++ src/Component/BookList/BookList.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BookList = () => {
+  const [books,setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [wishlist, setWishlist] = useState(
+    JSON.parse(localStorage.getItem('wishlist'))||[]
+  )
+  const [genre, setGenre] = useState('')
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   return (
     <div>
       <h1>Here is the book list</h1>
