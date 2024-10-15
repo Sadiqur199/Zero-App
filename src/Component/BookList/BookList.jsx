a21062973d2e320ca7ccc78fcb39fe66f4139897
Sadiqur199 <sadiqurrhaman199@gmail.com>
2024-10-16 03:21:41 +0600

    fetch api data

--- src/Component/BookList/BookList.jsx
+++ src/Component/BookList/BookList.jsx
@@ -10,6 +10,24 @@ const BookList = () => {
   const [genre, setGenre] = useState('')
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const fetchBooks = async (page=1) =>{
+    setLoading(true);
+    try{
+      const response = await fetch(`https://gutendex.com/books?page=${page}`)
+      const data = await response.json();
+      setBooks(data.results);
+      setTotalPages(Math.ceil(data.count / 32))
+    }
+    
+    catch(error){
+      console.error('Error Fetching:',error)
+    }
+    setLoading(false)
+  }
+  useEffect(() => {
+    fetchBooks(currentPage); 
+  }, [currentPage]);
+
   return (
     <div>
       <h1>Here is the book list</h1>
